e9c725a81f1dbd708571fff7772b6b7967fcf90c
Paula Murphy <paulam849@gmail.com>
2015-12-29 15:48:00 -0500

    first commit

---
+++ rubyrotate.rb
@@ -0,0 +1,42 @@
+def function_rotate(array, n)
+if else
+
+return
+
+end
+I do not understand how to write the function but I understand the concept. 
+# Lloyd heple me understand 
+# Step 1 In the array the first postion to be moved forward 2 steps
+# array
+# go throught the array grab the position of each element.
+# Step 2 add 2 more than 2 substract 2
+
+# beginning=[cat, dog, mouse, shoe] 
+# end [mouse, shoe, cat, dog]
+# a[2]mouse would be to b[0]mouse you neeed to subtact -2 
+
+ 
+#  a[0]=cat +2        
+#  a[1]=dog +2
+#  a[2]=mouse -2
+#  a[3]=shoe-2
+
+# b[0]=mouse
+# b[1]=shoe
+# b[2]=cat
+# b[3]=dog
+
+# Create an n factorial function. A factorial is the evaluation of n! - This number is calculated by multiplying every number from 1 to and including n.
+
+# this number is calucated by multiplying  every number from 1 and inclucing n
+
+7*((7-1))! is equal to 7*((6)!)
+
+def fact(n)
+	if n ==0
+		1
+	else
+	n* fact(n-1)
+end
+end
+print fact(8)		
